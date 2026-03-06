6a1ed5a5f9fa9ea5c01a607690db2f554a62fb0e
moded12 <mod6512@gmail.com>
2026-03-06 14:53:37 +0300

    fix deploy

--- deploy.php
+++ deploy.php
@@ -1,16 +1,5 @@
 <?php
 
-$secret = "123456";
-
-$payload = file_get_contents("php://input");
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
-
-$hash = 'sha1=' . hash_hmac('sha1', $payload, $secret);
-
-if (!hash_equals($hash, $signature)) {
-    die("Invalid signature");
-}
-
 $output = shell_exec("cd /var/www/vhosts/shneler.com/httpdocs/zakat && git pull origin main 2>&1");
 
 echo "<pre>$output</pre>";
